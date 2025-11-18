73a15f403164662f2f2f007948aee3d398605f9a
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-11-17 19:57:20 -0600

    Add initial database export with sample user data

---
+++ src/db.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    name: "Christopher",
+  },
+  {
+    id: 2,
+    name: "Alex",
+  },
+  {
+    id: 3,
+    name: "Jordan",
+  },
+  {
+    id: 4,
+    name: "Taylor",
+  },
+  {
+    id: 5,
+    name: "Morgan",
+  },
+];
