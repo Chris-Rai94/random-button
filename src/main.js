03ef6865911719b2d6d3e4b6e96d370431881355
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-11-17 20:21:54 -0600

    Refactor main.js to import data and set up event listener for random student selection

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import "./style.css";
-import "./db.js";
+
+// find a random id from the db.js file
+import data from "./db.js";
+
+const button = document.querySelector("button");
+const nameDisplay = document.querySelector("p");
+const body = document.querySelector("body");
+button.addEventListener("click", () => {
+  const randomPerson = data[Math.floor(Math.random() * data.length)];
+  nameDisplay.textContent = randomPerson.name;
+  body.style.backgroundColor = `hsl(${Math.random() * 360}, ${Math.random() * 100}%, ${Math.random() * 100}%)`;
+});
