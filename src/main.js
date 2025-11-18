dd404ec00c875c0079615b73c2f17832ac7adfcf
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-11-17 20:25:03 -0600

    Refactor random color generation to use a dedicated function for improved readability

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import "./style.css";
 
 // find a random id from the db.js file
 import data from "./db.js";
-
+const randomNumber = () => Math.random() * 100;
 const button = document.querySelector("button");
 const nameDisplay = document.querySelector("p");
 const body = document.querySelector("body");
 button.addEventListener("click", () => {
   const randomPerson = data[Math.floor(Math.random() * data.length)];
   nameDisplay.textContent = randomPerson.name;
-  body.style.backgroundColor = `hsl(${Math.random() * 360}, ${Math.random() * 100}%, ${Math.random() * 100}%)`;
+  body.style.backgroundColor = `hsl(${Math.random() * 360}, ${randomNumber()}%, ${randomNumber()}%)`;
 });
